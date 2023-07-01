88368618efeb62ba6441c3a40d78472d89981a18
Lorin Knudson <lcknudson@gmail.com>
2023-07-01 17:11:43 -0600

    testgoogle working

--- automation-demo/testgoogle/practiceTest.test.js
+++ automation-demo/testgoogle/practiceTest.test.js
@@ -1,25 +1,44 @@
-const { By, Builder, Browser, until, Key } = require("selenium-webdriver");
+const { By, Builder, Browser, until, Key, Capabilities } = require("selenium-webdriver");
 
-let driver;
+require("chromedriver");
 
-// Build a new driver for each test
-beforeEach(async () => {
-  driver = await new Builder().forBrowser(Browser.CHROME).build();
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
+
+const { search } = require('./search');
+
+beforeAll(async () => {
+  await (await driver).get("https://www.google.com/")
 });
+
+
+// Build a new driver for each test, this wasn't in John's demo, so I commented it out
+// beforeEach(async() => {
+//   let driver = await new Builder().forBrowser(Browser.CHROME).build();
+//   console.log('Before')
+// });
+
+
 
 // Quit a driver after each test
-afterEach(async () => {
-  await driver.quit();
+afterEach(async() => {
+  // if comment out the code below it will keep google open to see what it did
+  // await (await driver).quit();
+  console.log('after')
 });
 
-describe("Test the Google homepage", () => {
-  test("can search Google for 'Selenium'", async () => {
-    // Navigate to google.com
-    await driver.get("https://www.google.com/");
+// describe("Test the Google homepage", () => {
+//   test("can search Google for 'Selenium'", async () => {
+//     // Navigate to google.com
+//     await driver.get("https://www.google.com/");
 
-    // Locate the search bar and send the search term to it
-    await driver.findElement(By.name("q")).sendKeys("selenium", Key.RETURN);
-    // Wait until the title of the page changes to include the search term
-    await driver.wait(until.titleIs("selenium - Google Search"), 1000);
-  });
-});
+//     // Locate the search bar and send the search term to it
+//     await driver.findElement(By.name('q')).sendKeys("selenium", Key.RETURN);
+//     // Wait until the title of the page changes to include the search term
+//     await driver.wait(until.titleIs("selenium - Google Search"), 1000);
+//   });
+// });
+
+test("Google Search Test", async() => {
+  await search(driver, "Tenet")
+  console.log('googled Tenet')
+})
---
+++ automation-demo/testgoogle/search.js
@@ -0,0 +1,23 @@
+// Here we need to Import By, which will allow us to either css or xpat selectors in our code
+const { By } = require('selenium-webdriver')
+
+const search = async (driver, searchTerm) => {
+
+    //We are starting with an await since we're dealing with promises and the browers
+    //We have our automation find teh element with we can then use the senKeys method to type
+    //into that element, and the \n to hit enter after typing the search term
+    await driver.findElement(By.name('q')).sendKeys(`${searchTerm}\n`)
+
+    //We are then setting a variable to be the text from the results of our Google search
+    //Instaed of using the sendkeys method, we are using the getText method with will return
+    //the text in that element as a string
+    let resultsText = await driver.findElement(By.id('res')).getText()
+
+    console.log({ resultsText })
+    //We make our results lower case and then expect it to contain our original search term
+    expect(resultsText.toLowerCase()).toContain(searchTerm.toLowerCase())
+}
+
+module.exports = {
+    search
+}
